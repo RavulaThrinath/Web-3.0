5315cfdc679b990a35e7ec9376e9061efdeb49e8
RavulaThrinath-Git <thrinathravual@gmail.com>
2021-12-29 11:02:02 +0530

    commit

--- src/Components/Blog card/Blogcard.js
+++ src/Components/Blog card/Blogcard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Blogcard.css";
 import Coverpic from "../Assets/Blogcardpic.svg";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const CardContainer = styled.div`
   background-color: #ffffff;
@@ -17,39 +17,24 @@ const CoverImage = styled.img`
   margin: auto;
   border-radius: 12px;
 `;
-const PostDate = styled.h2`
-  font-size: 16px;
-  font-family: "Outfit", sans-serif;
-  letter-spacing: 0.3px;
-  font-weight: 500;
-  color: #1b1b1bb2;
-  margin: 2px 5px 10px 5px;
-  width: 260px;
-`;
 const BlogTitle = styled.h3`
   margin: 10px 5px 5px 5px;
   text-transform: capitalize;
   color: #000000;
-  width: 190px;
-  line-height: 25px;
-  font-size: 20px;
-  font-family: "Montserrat", sans-serif;
+  width: 250px;
+  line-height: 35px;
+  font-size: 22px;
+  font-family: "Outfit", sans-serif;
 `;
 
-const Blogcard = () => {
+const Blogcard = (props) => {
   return (
-    <Link to="/Nft">
       <div>
         <CardContainer className="card-container">
           <CoverImage className="img-container" src={Coverpic} alt="" />
-          <BlogTitle className="blog-title">NFTs</BlogTitle>
-          <PostDate className="Post-date">
-            An NFT is a digital asset that represents real-world objects like
-            art, music, game items and videos.
-          </PostDate>
+          <BlogTitle className="blog-title">{props.CardTitle}</BlogTitle>
         </CardContainer>
       </div>
-    </Link>
   );
 };
 
--- src/Components/Home/Articles.js/Article.js
+++ src/Components/Home/Articles.js/Article.js
@@ -1,44 +1,42 @@
 import React from "react";
 import Blogcard from "../../Blog card/Blogcard";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 // Styling Start
 const Main = styled.div`
   width: 100%;
-  margin-top:40px;
+  margin-top: 40px;
 `;
 
 const ArticleTitle = styled.div`
   & {
     position: relative;
-    font-size: 20px;
+    color: #393e46;
+    font-size: 25px;
+    margin: 60px 0 50px 0;
     font-family: "Cinzel Decorative", cursive;
     font-weight: 500;
-    color: #000000;
-    letter-spacing: 7px;
+    letter-spacing: 5px;
     text-transform: lowercase;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 70px 0 60px 0;
   }
   &:after {
     content: "";
     position: absolute;
-    top: 100px;
-    width: 190px;
-    height: .5px;
-    border-radius: 10px;
-    background: #00000021;
+    top: 35px;
+    width: 300px;
+    right: 8px;
+    height: 1px;
+    background: linear-gradient(to left, #b8b8b8a2, #f3f3f3);
   }
 `;
 const ArticleList = styled.div`
   display: flex;
   flex-wrap: wrap;
-  gap:40px;
+  gap: 40px;
   align-items: center;
-  max-width:1000px;
-  margin:auto;
+  max-width: 1000px;
+  margin: auto;
   justify-content: center;
 `;
 const ArrowButton = styled.div`
@@ -50,25 +48,34 @@ const Item = styled.div`
   width: 200px;
   height: 50px;
   margin: auto;
-`
+`;
 
-const Article = ({ containerTitle, buttonName }) => {
+
+const Article = ({ buttonName }) => {
   const navToBlog = () => {
     window.open("../../Blogs", "_self");
   };
 
-
-
   return (
     <Main className="main">
-      <ArticleTitle className="article-title">{containerTitle}</ArticleTitle>
+      <div className="title-main" style={{
+        // maxWidth: '910px',
+        display:'flex',
+        justifyContent:'end',
+        marginRight:'145px'
+
+    }}>
+        <ArticleTitle className="article-title">Latest Blogs</ArticleTitle>
+      </div>
       <ArticleList className="blog-list">
-        <Blogcard  />
-        <Blogcard  />
-        <Blogcard  />
-        <Blogcard  />
-        <Blogcard  />
-        <Blogcard  />
+        <Link to="/nft" style={{ textDecoration: "none" }}>
+          <Blogcard CardTitle="What You Need To Know About Non-Fungible Tokens (NFTs)" />
+        </Link>
+
+        <Link to="/Blockchain" style={{ textDecoration: "none" }}>
+          <Blogcard CardTitle="What You Need To Know About Non-Fungible Tokens (NFTs)" />
+        </Link>
+        <Blogcard CardTitle="What You Need To Know About Blockchain Technology" />
       </ArticleList>
       <ArrowButton className="arrow-btn">
         <Item class="item">
